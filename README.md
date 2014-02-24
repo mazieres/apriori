806772fbfed4fae58bbb02926a7e3c3ceba70984
Antoine Mazières <antoine.mazieres@gmail.com>
2014-02-24 11:09:56 +0100

    first

--- README.md
+++ README.md
@@ -1,4 +1,46 @@
-apriori
-=======
+# Apriori
 
-A simple version of the apriori algorithm, one item only per event.
+## About
+
+A simple implementation of the [apriori algorithm](https://en.wikipedia.org/wiki/Apriori_algorithm). A method for extracting frequent substructures in a set of sequences of ordered events.
+
+## How To
+
+`Apriori` takes a list of strings, representing sequences, and an integer, representing the percentage of sequences the pattern must match for being considered.
+
+```python
+In [1]: from apriori import *
+
+In [2]: data = ["ABCDEFGHIJKL","ZOPQABCDLMNOP","REWQZOPQAB"]
+
+In [3]: patterns = Apriori(data, 34)
+
+In [4]: patterns
+Out[4]:
+{'AB': 3,
+ 'ABC': 2,
+ 'ABCD': 2,
+ 'BC': 2,
+ 'BCD': 2,
+ 'CD': 2,
+ 'OP': 2,
+ 'OPQ': 2,
+ 'OPQA': 2,
+ 'OPQAB': 2,
+ 'PQ': 2,
+ 'PQA': 2,
+ 'PQAB': 2,
+ 'QA': 2,
+ 'QAB': 2,
+ 'ZO': 2,
+ 'ZOP': 2,
+ 'ZOPQ': 2,
+ 'ZOPQA': 2,
+ 'ZOPQAB': 2}
+ ```
+
+## References
+
++ "Mining Frequent Patterns, Associations, and Correlations" (Chap. 5) *in* Han, J., Kamber, M., & Pei, J. (2006). **Data mining: concepts and techniques.** Morgan kaufmann.
+
++ Mooney, C. H., & Roddick, J. F. (2013). **Sequential pattern mining--approaches and algorithms.** ACM Computing Surveys (CSUR), 45(2), 19.
---
+++ apriori.py
@@ -0,0 +1,60 @@
+import re
+from collections import defaultdict
+
+
+class Apriori(dict):
+	"""A simple implementation of the Apriori algorithm.
+
+	It takes only sequences of 1-item events.
+
+	"""
+
+	def __init__(self, listOfSequences, support):
+		"""Performs apriori algorithm on `listOfSequences` with `support` as a threshold.
+
+		Args:
+			listOfSequences (list): A list of strings, each letter representing a specific event.
+			support (int): The minimum percentage of sequences a pattern must match.
+
+		"""
+
+		super(Apriori, self).__init__()
+		self.data = listOfSequences
+		self.thres = (support * len(self.data)) / 100.0
+		self.primitives = self.getPrimitives()
+		self.apriori()
+		del self.data
+
+	def apriori(self):
+		candidates = self.getNewCandidates(self.primitives)
+		while len(candidates) > 0:
+			res = self.getPatternsCount(candidates)
+			self.update(res)
+			candidates = self.getNewCandidates(res.keys())
+		
+	def getPrimitives(self):
+		primitives = set()
+		for seq in self.data:
+			for event in seq:
+				primitives.add(event)
+		return primitives
+
+	def getNewCandidates(self, candidates):
+		newCandidates = set()
+		for seq in self.data:
+			for can in candidates:
+				for subs in re.findall(can+".", seq):
+					newCandidates.add(subs)
+		return newCandidates
+
+	def getPatternsCount(self, candidates):
+		patternsCount = defaultdict(int)
+		for seq in self.data:
+			for can in candidates:
+				if can in seq:
+					patternsCount[can] += 1
+		return {k:v for k, v in patternsCount.iteritems() if v > self.thres}
+
+
+if __name__ == '__main__':
+	pass
